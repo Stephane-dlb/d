09d80f89a1852855d835965ba39a9dae0c26e74d
Stephane-dlb <Stephane-dlb@users.noreply.github.com>
2021-10-07 18:41:37 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="Stephane-dlb"
+original_name="d"
+original_urlname="d"
+original_description="Awesome d created by Stephane-dlb"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv d $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
